62d7ade237d2ad67ad36c6393c8e032dd67eb690
powpowdan <powpowdan@gmail.com>
2024-07-16 22:37:09 -0400

    completed

--- index.js
+++ index.js
@@ -41,34 +41,39 @@ app.get("/basicAuth", async (req, res) => {
  
 
  
-app.get("/apiKey", (req, res) => {
-
-
-  //TODO 4: Write your code here to hit up the /filter endpoint
+app.get("/apiKey", async (req, res) =>  {
+    //TODO 4: Write your code here to hit up the /filter endpoint
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
-
-
+  try {
+    const result = await axios.get(`${API_URL}/filter?score=5&apiKey=${yourAPIKey}` );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
-app.get("/bearerToken", (req, res) => {
 
 
-  //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
-  //and get the secret with id of 42
-  //HINT: This is how you can use axios to do bearer token auth:
-  // https://stackoverflow.com/a/52645402
-  /*
-  axios.get(URL, {
-    headers: { 
-      Authorization: `Bearer <YOUR TOKEN HERE>` 
-    },
-  });
-  */
 
 
+ 
+
+app.get("/bearerToken", async (req, res) => {
+
+  try {
+    const result = await axios.get(`${API_URL}/secrets/42`, {
+      headers: { 
+        Authorization: `Bearer ${yourBearerToken}` 
+      },
+    } );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
-
+  
+ 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
